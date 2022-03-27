ba559dad74f6dc888d549cb29a9b754629c52a7b
vanhoanhuynh <vanhoan2006@gmai.com>
2022-03-27 11:01:20 +0700

    add some new selenium method

--- base.py
+++ base.py
@@ -6,3 +6,17 @@ driver = webdriver.Chrome(ChromeDriverManager().install())
 # navigate to google page
 driver.get("https://google.com")
 
+title = driver.title
+
+print("Title of google page is: ", title)
+
+#search = driver.find_element_by_class_name("q")
+
+#print("result of search: ", search)
+
+page_source = driver.page_source
+
+print("page source of google page is: ", page_source)
+
+driver.quit()
+
