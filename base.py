28dbdeac254ad468467fe49861b2f9052c28fd76
vanhoanhuynh <vanhoan2006@gmai.com>
2022-04-03 10:53:06 +0700

    add new line and searchproducts file

--- base.py
+++ base.py
@@ -1,22 +1,35 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
-
+driver.maximize_window()
 # navigate to google page
-driver.get("https://google.com")
+# navigate to the application home page
+driver.get("https://www.w3schools.com/")
 
 title = driver.title
 
 print("Title of google page is: ", title)
 
+#login = driver.find_element_by_id("w3loginbtn")
+
+
+# driver.find_element(By.XPATH, '//button[text()="Some text"]')
+# driver.find_elements(By.XPATH, '//button')
+
+login = driver.find_element(By.ID, 'w3loginbtn')
+
+login.click()
+
 #search = driver.find_element_by_class_name("q")
 
 #print("result of search: ", search)
 
-page_source = driver.page_source
+#page_source = driver.page_source
 
-print("page source of google page is: ", page_source)
+#print("page source of google page is: ", page_source)
 
-driver.quit()
+#driver.quit()
 
---
+++ searchproducts.py
@@ -0,0 +1,10 @@
+import os
+from selenium import webdriver
+
+# get the path of chromedriver
+dir = os.path.dirname(__file__)
+
+print("path is: ", dir)
+
+# create a new 
+#driver = webdriver
