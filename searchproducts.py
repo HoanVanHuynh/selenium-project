786a75d3937e4b366b0d4fd14d1003c27d8609da
vanhoanhuynh <vanhoan2006@gmai.com>
2022-04-03 10:57:18 +0700

    add some line to searchproducts

--- searchproducts.py
+++ searchproducts.py
@@ -3,6 +3,14 @@ from selenium import webdriver
 
 # get the path of chromedriver
 dir = os.path.dirname(__file__)
+chrome_driver_path = dir + "\chromedriver.exe" #remove the .exe extension on linux or mac platform
+
+# create a new Chrome session
+driver = webdriver.Chrome(chrome_driver_path)
+driver.implicitly_wait(30)
+driver.maximize_window()
+
+
 
 print("path is: ", dir)
 
